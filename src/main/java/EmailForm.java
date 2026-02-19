65acee6607a9d22aa961420d0e7faf5994239a15
Oran Flanagan <G00428429@atu.ie>
2026-02-19 13:46:45 +0000

    EmailForm model created

---
+++ src/main/java/EmailForm.java
@@ -0,0 +1,13 @@
+public class EmailForm {
+    private Float ticketID;
+    private String subject;
+    private String body;
+
+    // Getters and Setters
+    public Float getTicketID() { return ticketID; }
+    public void setTicketID(Float ticketID) { this.ticketID = ticketID; }
+    public String getSubject() { return subject; }
+    public void setSubject(String subject) { this.subject = subject; }
+    public String getBody() { return body; }
+    public void setBody(String body) { this.body = body; }
+}
