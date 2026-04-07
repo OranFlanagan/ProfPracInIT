a96e76209841b3be4e399fead6a356523671c750
Roanbradley <roanbradley5@gmail.com>
2026-04-07 13:29:01 +0100

    added suportpageController

---
+++ src/main/java/com/TicketingApp/Controller/supportPageController.java
@@ -0,0 +1,20 @@
+package com.TicketingApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class supportPageController {
+
+
+@RequestMapping("/support-page")
+public String getSupportPage() {
+    return "support-page";
+}
+
+ 
+
+
+}
